403235fd364dfd58dc447c82881bb49c9034c981
Alireza Darban <alirezadarban@vanishingpoint.com.au>
2020-10-19 05:48:29 +1100

    colorize the output and set some error handlings

--- server.js
+++ server.js
@@ -9,37 +9,56 @@ let setting = JSON.parse(settingRawdata);
 
 var server= http.createServer((function(request,response)
 {
-
+    //
+    //'Content-Type': 'text/plain; charset=utf-8',
+    //'Content-Type': 'text/html; charset=UTF-8',
     response.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8',
+        'Content-Type': 'text/html; charset=utf-8',
         'Transfer-Encoding': 'chunked',
         'X-Content-Type-Options': 'nosniff'
     });
 
-    console.log(request.url);
+    console.log(request.connection.remoteAddress + " - " + request.url);
 	let settingRawdata = fs.readFileSync('setting.json');
 	let setting = JSON.parse(settingRawdata);
 
-    if (request.url==="/"){
-        response.end("Please send the parameter");
-        return;
+    try {
+        // const regex = RegExp(/\/[A-Za-z_][\w\-]*/);
+        let regexResult = request.url.match(/\/[A-Za-z_][\w\-]*/);
+        let [discovered] = regexResult;
+        if(discovered !== request.url)
+        {
+            console.log("Bad parameter - Nothing has ran...")
+            response.end("");  // bad parameter
+            return;
+        }
+
+        // if (request.url==="/"){
+        //     response.end("2");//Please send the parameter
+        //     return;
+        // }
+        
+        let script= setting.scripts.find(x => "/"+x.key===request.url);
+        if (!script){
+            console.log("Not found - Nothing has ran...")
+            response.end(""); //No items found
+            return;
+        }
+
+        if (!script.isActive){
+            console.log("Is not enabled - Nothing has ran...")
+            response.end(""); //script is not enabled
+            return;
+        }
+
+        response.write('<body style="font-family : Courier New"> Beginning\n');
+
+        exec(script.app,[script.path], response);
+    } catch(e) {
+        console.log("Error while running the job...\n" + e);
+        response.end("");  // bad parameter
+
     }
-
-    let script= setting.scripts.find(x => "/"+x.key===request.url);
-    if (!script){
-        response.end("No items found");
-        return;
-    }
-
-    if (!script.isActive){
-        response.end("script is not enabled");
-        return;
-    }
-
-    response.write('Beginning\n');
-
-    exec(script.app,[script.path], response);
-
     // response.end();
     // response.end("Finished\n");
 }));
@@ -55,19 +74,22 @@ function exec(commandStr,params, response){
     const command = spawn(commandStr,params);
     
     command.stdout.on("data", data => {
-        response.write(`${data}`);
+        let newData = data.toString().replace("\n","</br>");
+        response.write(`${newData}`);
     });
     
     command.stderr.on("data", data => {
-        response.write(`stderr: ${data}\n`);
+        response.write(`<div style="color : orange">${data}</div>\n`);
     });
+
+    command
     
     command.on('error', (error) => {
-        response.write(`error: ${error.message}\n`);
+        response.write(`<div style="color : red">${error.message}</div>\n`);
     });
     
     command.on("close", code => {
-        response.end(`Process finished with ${code},[]\n`);
+        response.end(`<div style="color : green">Process finished with ${code}</div>\n`);
     });
 
 }
